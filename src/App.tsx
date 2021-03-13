1c7b62ba71d10fb59a765511dcce2aa6149d7a1b
vishnuk7 <vishnu.kraman88@gmail.com>
2021-03-13 23:13:59 +0530

    transpile working

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './styles/index.css';
+import * as esbulid from 'esbuild-wasm';
 
 const App = () => {
 	const [input, setInput] = useState('');
 	const [code, setCode] = useState('');
 
-	const onClick = () => {
-		console.log(input);
+	const startService = async () => {
+		await esbulid.initialize({
+			worker: true,
+			wasmURL: '/esbuild.wasm',
+		});
+	};
+
+	useEffect(() => {
+		startService();
+	}, []);
+
+	const onClick = async () => {
+		const result = await esbulid.transform(input, {
+			loader: 'jsx',
+			target: 'es2015',
+		});
+
+		setCode(result.code);
 	};
 
 	return (
@@ -15,6 +32,7 @@ const App = () => {
 			<button onClick={onClick} className='bg-red-600 text-white py-1 px-3 rounded-md'>
 				Submit
 			</button>
+			<p>{code}</p>
 		</div>
 	);
 };
