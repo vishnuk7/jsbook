7e259cf4fe09f8d96c3a58c59c43f667db29c7ea
vishnuk7 <vishnu.kraman88@gmail.com>
2021-03-13 22:39:56 +0530

    adding wasm file into public folder ✨

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { useState } from 'react';
 import './styles/index.css';
 
-function App() {
-	return <div className='App bg-yellow-300'>Hello</div>;
-}
+const App = () => {
+	const [input, setInput] = useState('');
+	const [code, setCode] = useState('');
+
+	const onClick = () => {
+		console.log(input);
+	};
+
+	return (
+		<div>
+			<textarea onChange={(e) => setInput(e.target.value)} className='border-gray-400 border-2'></textarea>
+			<button onClick={onClick} className='bg-red-600 text-white py-1 px-3 rounded-md'>
+				Submit
+			</button>
+		</div>
+	);
+};
 
 export default App;
