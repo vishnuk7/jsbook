5a36e22b34531c6f897fbd78c95bac40166d3cba
vishnuk7 <vishnu.kraman88@gmail.com>
2021-03-14 16:21:00 +0530

    caching using indexdb ⚡

--- src/unpkg-path-plugin.ts
+++ src/unpkg-path-plugin.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
+import localforage from 'localforage';
+
+const fileCache = localforage.createInstance({
+	name: 'fileCache',
+});
 
 export const unpkgPathPlugin = () => {
 	return {
@@ -40,13 +45,23 @@ export const unpkgPathPlugin = () => {
 					};
 				}
 
+				const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+
+				if (cachedResult) {
+					return cachedResult;
+				}
+
 				const { data, request } = await axios.get(args.path);
 
-				return {
+				const result: esbuild.OnLoadResult = {
 					loader: 'jsx',
 					contents: data,
 					resolveDir: new URL('./', request.responseURL).pathname,
 				};
+
+				await fileCache.setItem(args.path, result);
+
+				return result;
 			});
 		},
 	};
