ad7de3fb1bc6842bb6dc34b977a3805dfa574945
vishnuk7 <vishnu.kraman88@gmail.com>
2021-03-18 02:43:24 +0530

    code editor done 🔥

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,21 @@ import { fetchPlugin } from './plugin/fetch-plugin';
 import { CodeEditor } from './components/CodeEditor';
 
 const App = () => {
-	const [input, setInput] = useState('');
-	const iframe = useRef<HTMLIFrameElement>(null);
+    const [input, setInput] = useState('');
+    const iframe = useRef<HTMLIFrameElement>(null);
 
-	const startService = async () => {
-		await esbulid.initialize({
-			worker: true,
-			wasmURL: 'https://unpkg.com/esbuild-wasm@0.9.2/esbuild.wasm',
-		});
-	};
+    const startService = async () => {
+        await esbulid.initialize({
+            worker: true,
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.9.2/esbuild.wasm',
+        });
+    };
 
-	useEffect(() => {
-		startService();
-	}, []);
+    useEffect(() => {
+        startService();
+    }, []);
 
-	const html = `
+    const html = `
 		<html>
 			<head></head>
 			<body>
@@ -41,34 +41,52 @@ const App = () => {
 		</html>
 	`;
 
-	if (iframe && iframe.current) iframe.current.srcdoc = html;
+    if (iframe && iframe.current) iframe.current.srcdoc = html;
 
-	const onClick = async () => {
-		const result = await esbulid.build({
-			entryPoints: ['index.js'],
-			bundle: true,
-			write: false,
-			plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-			define: {
-				'process.env.NODE_ENV': '"production"',
-				global: 'window',
-			},
-		});
+    const onClick = async () => {
+        const result = await esbulid.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window',
+            },
+        });
 
-		if (iframe && iframe.current && iframe.current.contentWindow)
-			iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-	};
+        if (iframe && iframe.current && iframe.current.contentWindow)
+            iframe.current.contentWindow.postMessage(
+                result.outputFiles[0].text,
+                '*'
+            );
+    };
 
-	return (
-		<div>
-			<CodeEditor />
-			<textarea onChange={(e) => setInput(e.target.value)} className='border-gray-400 border-2'></textarea>
-			<button onClick={onClick} className='bg-red-600 text-white py-1 px-3 rounded-md'>
-				Submit
-			</button>
-			<iframe title='code-preview' ref={iframe} sandbox='allow-scripts' srcDoc={html} />
-		</div>
-	);
+    return (
+        <div>
+            <CodeEditor
+                onEditorChange={(value) => setInput(value)}
+                initialValue="const a = 2"
+            />
+            <textarea
+                onChange={(e) => setInput(e.target.value)}
+                className="border-gray-400 border-2"
+            ></textarea>
+            <button
+                onClick={onClick}
+                className="bg-red-600 text-white py-1 px-3 rounded-md"
+            >
+                Submit
+            </button>
+            <code>{input}</code>
+            <iframe
+                title="code-preview"
+                ref={iframe}
+                sandbox="allow-scripts"
+                srcDoc={html}
+            />
+        </div>
+    );
 };
 
 export default App;
--- src/components/CodeEditor.tsx
+++ src/components/CodeEditor.tsx
@@ -1,22 +1,98 @@
-import MoancoEditor from '@monaco-editor/react';
-import React from 'react';
+import MoancoEditor, { OnChange, OnMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import codeShift from 'jscodeshift';
+import Highlighter from 'monaco-jsx-highlighter';
+import styled from 'styled-components';
+import React, { useRef } from 'react';
 
-export const CodeEditor: React.FC = () => {
-	return (
-		<MoancoEditor
-			theme='vs-dark'
-			height='300px'
-			language='javascript'
-			options={{
-				wordWrap: 'on',
-				minimap: { enabled: false },
-				showUnused: false,
-				lineNumbersMinChars: 3,
-				folding: false,
-				fontSize: 16,
-				scrollBeyondLastLine: false,
-				automaticLayout: true,
-			}}
-		/>
-	);
+interface CodeEditorProps {
+    initialValue: string;
+    onEditorChange(value: string): void;
+}
+
+export const CodeEditor: React.FC<CodeEditorProps> = ({
+    initialValue,
+    onEditorChange,
+}) => {
+    const editorRef = useRef<any>();
+
+    const handleChange: OnChange = (getValue, ev): void => {
+        getValue && onEditorChange(getValue);
+
+        const highlighter = new Highlighter(
+            //@ts-ignore
+            window.monaco,
+            codeShift,
+            editorRef
+        );
+
+        highlighter.highLightOnDidChangeModelContent();
+    };
+
+    const onFormatClick = () => {
+        const unformatted = editorRef.current.getModel()?.getValue();
+        console.log(unformatted);
+
+        const formatted = prettier
+            .format(unformatted, {
+                parser: 'babel',
+                plugins: [parser],
+                useTabs: false,
+                semi: true,
+                singleQuote: true,
+            })
+            .replace(/\n$/, '');
+
+        editorRef.current.setValue(formatted);
+    };
+
+    const onEditorMount: OnMount = (editor, monaco) => {
+        editorRef.current = editor;
+        console.log(editor);
+    };
+
+    return (
+        <EditorStyled>
+            <div className="absolute z-10 top-1.5 right-1.5">
+                <button
+                    className="bg-red-600 text-gray-300 px-4 py-2 rounded-sm hover:bg-red-700"
+                    onClick={onFormatClick}
+                >
+                    Format
+                </button>
+            </div>
+            <MoancoEditor
+                onChange={handleChange}
+                onMount={onEditorMount}
+                value={initialValue}
+                theme="vs-dark"
+                height="300px"
+                language="javascript"
+                options={{
+                    wordWrap: 'on',
+                    minimap: { enabled: false },
+                    showUnused: false,
+                    lineNumbersMinChars: 3,
+                    folding: false,
+                    fontSize: 16,
+                    scrollBeyondLastLine: false,
+                    automaticLayout: true,
+                    tabSize: 2,
+                }}
+            />
+        </EditorStyled>
+    );
 };
+
+const EditorStyled = styled.div`
+    button {
+        opacity: 0;
+        transition: all 0.2 ease-in;
+    }
+    &:hover {
+        button {
+            opacity: 1;
+        }
+    }
+`;
