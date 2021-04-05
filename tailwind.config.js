1edc82833d8d36997f5a77b454abad7680cbd0c3
vishnuk7 <vishnu.kraman88@gmail.com>
2021-04-06 03:00:37 +0530

    tailwind omptimize 🚀

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    purge: {
+        content: ['./src/*.js', './src/*.jsx', './src/*.ts', './src/*.tsx'],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
