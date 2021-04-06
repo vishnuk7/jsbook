da3069baa1a4dd7dba6f8e1636f8df227c452279
vishnuk7 <vishnu.kraman88@gmail.com>
2021-04-07 00:24:14 +0530

    tailwindcss build fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    purge: {
-        content: ['./src/*.js', './src/*.jsx', './src/*.ts', './src/*.tsx'],
-    },
+    purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
